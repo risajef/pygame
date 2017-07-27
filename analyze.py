6976c3d038ca8a45c612d83f83f788df19121687
risajef <reto.a.weber@gmail.com>
2017-07-27 16:22:46 +0200

    debug and added analyzer

--- analyze.py
+++ analyze.py
@@ -309,48 +309,9 @@ class Mine:
         self.circle.draw(self.color)
 
 
-GROW_APPLE, t = pygame.USEREVENT+1, 100
-pygame.time.set_timer(GROW_APPLE, t)
-
-KILL, t = pygame.USEREVENT+2, 1000
-pygame.time.set_timer(KILL, t)
-
-HURT, t = pygame.USEREVENT+3, 20
-pygame.time.set_timer(HURT, t)
-
-# Amount of Frames after which the event is called
-GROW_APPLE = 30
-KILL = 30
-HURT = 7
-
-
-
-
-
 def gameLoop():
-    print_los = False
-    view_scene = True
-    FPS = 60
-    t = 0
     gameExit = False
     gameOver = (False, None)
-    temp = 0
-    
-    messages = []
-       
-    players = [Walker() for _ in range(10)]
-    apples = [Apple() for _ in range(8)]
-    mines = [Mine() for _ in range(30)]
-    global cheap_distance_data_structure
-#    for p in players:
-#        cheap_distance_data_structure[p.x // line_of_sight, p.y // line_of_sight].append(p)
-
-#    for a in apples:
-#        cheap_distance_data_structure[a.x // line_of_sight, a.y // line_of_sight].append(a)
-
-#    for m in mines:
-#        cheap_distance_data_structure[m.x // line_of_sight, m.y // line_of_sight].append(m)
-
 
     
     
@@ -364,7 +325,7 @@ def gameLoop():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
                         gameExit = True
-                        gameOver = False
+                        gameOver = (False, None)
                     if event.key == pygame.K_c:
                         gameLoop()
             
@@ -372,190 +333,21 @@ def gameLoop():
             if event.type == pygame.QUIT:
                 gameExit = True
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    FPS += 10
-                    print(FPS)
-                if event.key == pygame.K_DOWN:
-                    FPS -= 10
-                    print(FPS)
-                    
-                if event.key == pygame.K_RETURN:
-                    oldest = min([p.birth for p in players])
-                    print('l0', p.brain.l0)
-                    print('l1', p.brain.l1)
-                    print('l2', p.brain.l2)
-                    print('syn0', p.brain.syn0)
-                    print('syn1', p.brain.syn1)
-                    print('age' , time.time() - p.birth)
-                if event.key == pygame.K_s:
-                    with open("savegame", "wb") as f:
-                        data = [players, apples, mines, cheap_distance_data_structure]
-                        pickle.dump(data, f)
-                    print("saved!")
                 if event.key == pygame.K_l:
                    with open("savegame", "rb") as f:
                        players, apples, mines, cheap_distance_data_structure = pickle.load(f)
+                       player = players[0]
                    print("loaded!")
-                if event.key == pygame.K_v:
-                    print_los = not print_los
-                if event.key == pygame.K_SPACE:
-                    view_scene = not view_scene
-                if event.key == pygame.K_TAB:
-                    print(clock.get_fps())
-
-
-            # USEREVENTS
-            if t % GROW_APPLE == 0:
-#            if event.type == GROW_APPLE:
-                apple = Apple()
-                apples.append(apple)
-                #cheap_distance_data_structure[apple.index].append(apple)
-                
-#                if random.random() < (1/(2+len(apples))):
-            if t % KILL == -1:
-#            if event.type == KILL:
-                number_of_players = len(players)
-                if number_of_players > 20:
-                    births = [p.birth for p in players]
-                    now = time.time()
-                    age = [now - b for b in births]
-                    oldest = max(age)
-                    probability_of_dying = [a / oldest for a in age]
-                    for i, p in enumerate(players):
-                        if random.random() > probability_of_dying[i]:
-                            players.remove(p)
-                            cheap_distance_data_structure[p.index].remove(p)
-                else:
-                    for i in range(20 - number_of_players):
-                        players.append(Walker())
-                        messages.append(make_message(p, "new", t))
-                        print("new")
-                            
-            if t % HURT == 0:
-#            if event.type == HURT:
-                for p in players:
-                    p.health -=  1
-                    p.color.b = min(p.health, 255)
-                    p.color = pygame.Color(int(p.brain.l00[-1]*255), int(p.brain.l01[-1]*255), int(p.brain.l02[-1]*255))
-                    if p.health <= 0:
-                        players.remove(p)
-                        cheap_distance_data_structure[p.index].remove(p)
-                        messages.append(make_message(p, "hunger", t))
-                    if len(players) < 20:
-                        players.append(Walker(color = pygame.Color("blue")))
-                        messages.append(make_message(p, "new", t))
-                        print("new")
-                
-            
-                
-        
-        
-        # display Background
-        gameDisplay.fill(pygame.Color("black"))
-        
-        
-        break_var = False
-        
-        for p in players:
-            p.set_x(p.x + int(round(p.speed * math.cos(p.angle))))
-            p.set_y(p.y + int(round(p.speed * math.sin(p.angle))))
-            if p.angular_speed < -angular_speed:
-                p.angular_speed = -angular_speed
-            elif p.angular_speed > angular_speed:
-                p.angular_speed = angular_speed
-            p.angle += p.angular_speed
-            p.angle %= math.pi*2
-            
-            p.sight = [pygame.Color("black") for _ in range(p.resolution)]
-            
-            #cheap_distance_data_structure
-            
-            #print(near_objects(p, cheap_distance_data_structure))
-            for neighbor in near_objects(p, cheap_distance_data_structure):
-                dist = round(cheap_distance(neighbor.pos, p.pos))
-                #dist = round(distance(neighbor.pos, p.pos))
-                if dist < line_of_sight:                                    # close enough
-                    if type(neighbor) == Apple:    
-                        if p.is_in_view(neighbor.pos):                      # in front
-                            for subview in range(p.resolution):
-                                if p.is_in_subview(neighbor.pos, subview):  # in subview
-                                        p.sight[subview].g = max(int(round(255* (1 - dist / line_of_sight))), p.sight[subview].g)
-                                        
-                        if collision(neighbor.circle, p.circle):
-                            temp += 1
-                            print(temp)
-                            apples.remove(neighbor)
-                            cheap_distance_data_structure[neighbor.index].remove(neighbor)
-                            p.health += 50
-                            p.color.b = min(p.health, 255)
-                            for _ in range(1):
-                                cp = walker_copy(p)
-                                players.append(cp)
-                                            
-                    elif type(neighbor) == Mine:
-                        if p.is_in_view(neighbor.pos):                      # in front
-                            for subview in range(p.resolution):
-                                if p.is_in_subview(neighbor.pos, subview):  # in subview
-                                        p.sight[subview].r = max(int(round(255* (1 - dist / line_of_sight))), p.sight[subview].r)
-                    
-                        if collision(neighbor.circle, p.circle):
-                            temp -= 1
-                            print(temp)
-                            messages.append(make_message(p, "boom", t))
-                            mines.remove(neighbor)
-                            cheap_distance_data_structure[neighbor.index].remove(neighbor)
-                            mine = Mine()
-                            mines.append(mine)
-                            #cheap_distance_data_structure[mine.index].append(mine)
-                            if p in players:
-                                players.remove(p)
-                                cheap_distance_data_structure[p.index].remove(p)
-                                break_var = True
-            
-             
-            if break_var:
-                break_var = False
-                continue
-            
-            # drawangularline(gameDisplay, black, p.pos, p.angle, line_of_sight/10)
-            
-            p.brain.update(p.sight, p.head)
+                if event.key == pygame.K_RETURN:
+                    while(True):
+                        for i,s in enumerate(player.sight):
+                            inp = input("input " + str(i) + " : ")
+                            inp = inp.split()
+                            inp = [int(e) for e in inp]
+                            player.sight[i] = pygame.Color(inp[0], inp[1], inp[2])
                         
-            stop_brain = False
-            if not stop_brain:
-                p.speed = p.brain.l2[0] * speed
-                p.angular_speed = (p.brain.l2[1] - 0.5) * 2 * angular_speed
-                p.head = (p.brain.l2[2] - 0.5) * 2
-            
-            
-        if view_scene:
-                    
-            if print_los:
-                for p in players:
-                    for subview in range(p.resolution):
-                        p.draw_los_subview(p.sight[subview], subview) 
-
-            for a in apples:
-                a.draw()
-            
-            for m in mines:
-                m.draw()
-            
-            write_message(messages)
-            for m in messages:
-                if t - 30 > m[2]:
-                    messages.remove(m)
-            
-            for p in players:
-                p.draw()
-                        
-            pygame.display.update()
-        
-        clock.tick(FPS)
-        t += 1
-        #print(temp)
-        
-
+                        player.brain.update(player.sight, player.head)
+                        print(player.brain.l2)
 
     pygame.quit()
     quit()
